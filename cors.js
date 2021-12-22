6d973e9b586bf3b96eed485f891ac527cc299f25
AllaSydoriak <alochka12@ukr.net>
2021-12-23 00:19:22 +0200

    stable docker compose version

--- cors.js
+++ cors.js
@@ -1,5 +1,7 @@
 import cors from 'cors';
 
+console.log(process.env.NODE_ENV);
+
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production' ? process.env.origin : '*',
     methods: '*',
